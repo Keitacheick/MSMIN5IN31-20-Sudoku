a4676f37a1505eb5fa5fe65aa5ba9e5d93e37f81
Fabien SOLDE <fabien.solde@epfedu.fr>
2020-10-23 09:40:21 +0200

    First Commit

---
+++ NorvigSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NorvigSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
